e44f888502e34af75ef42f59fe763d73c9aa156b
shubham9194 <sbm.msra@gmail.com>
2017-08-05 12:02:41 +0530

    my first push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import  './App.css'
+class App extends Component {
+  constructor(){
+    super()
+  }
+
+  render() {    
+    return (
+      <div className="App TextAlignCenter">
+        hello
+      </div>
+    );
+  }
+}
+
+export default App;
